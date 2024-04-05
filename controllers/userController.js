f459e02b58bb65c5a645dfd5f6796d3574d43c79
triptisingla <triptisingla306@gmail.com>
2024-04-06 01:32:23 +0530

    test

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,34 +3,78 @@ import { User } from "../models/userSchema.js";
 
 export const register = async (req, res) => {
     const { name, email, password } = req.body;
+    // if (!name || !email || !password) {
+    //     return res.status(500).send({ message: 'Please fill full form!' })
+    // }
+    // const isEmail = await User.findOne({ email });
+    // if (isEmail) {
+    //     return res.status(500).send({ message: 'Email already registered!' })
+    // }
+    // const user = await User.create({
+    //     name,
+    //     email,
+    //     password,
+    // });
+    // return res.status(200).send({ data: 'User Registered!' })
+
+    // Detailed validation
     if (!name || !email || !password) {
-        return res.status(500).send({ message: 'Please fill full form!' })
+        return res.status(400).send({ message: 'Please fill out the entire form!' });
     }
+    if (!validator.isEmail(email)) {
+        return res.status(400).send({ message: 'Invalid email format.' });
+    }
+    if (password.length < 4 || password.length > 32) {
+        return res.status(400).send({ message: 'Password must be between 4 and 32 characters.' });
+    }
+    if (name.length < 3 || name.length > 30) {
+        return res.status(400).send({ message: 'Name must be between 3 and 30 characters.' });
+    }
+
     const isEmail = await User.findOne({ email });
     if (isEmail) {
-        return res.status(500).send({ message: 'Email already registered!' })
+        return res.status(409).send({ message: 'Email already registered!' });
     }
-    const user = await User.create({
-        name,
-        email,
-        password,
-    });
-    return res.status(200).send({ data: 'User Registered!' })
+
+    try {
+        const user = await User.create({
+            name,
+            email,
+            password,
+        });
+        return res.status(201).send({ data: 'User Registered!' });
+    } catch (error) {
+        return res.status(500).send({ message: 'Error registering user.' });
+    }
 };
 
 
 export const update = async (req, res) => {
+    const { email, name, password } = req.body; // Assuming these are the fields you want to allow updating
 
-    const newUser = req.body;
-    const email = newUser.email;
-    if (!newUser.email)
-        return res.status(500).send({ message: 'Please enter Email!' })
-
-    const isEmail = await User.findOne({ email });
-    if (!isEmail) {
-        return res.status(500).send({ message: "Email not present!" });
+    if (!email || !validator.isEmail(email)) {
+        return res.status(400).send({ message: 'Valid email is required for update.' });
     }
-    const updatedUser = await User.updateOne({ email }, newUser);
-    return res.status(200).send({ data: 'User Updated!' })
 
+    // Optional: Validate name and password if they are being updated
+    if (name && (name.length < 3 || name.length > 30)) {
+        return res.status(400).send({ message: 'Name must be between 3 and 30 characters if provided.' });
+    }
+    if (password && (password.length < 4 || password.length > 32)) {
+        return res.status(400).send({ message: 'Password must be between 4 and 32 characters if provided.' });
+    }
+
+    try {
+        const userToUpdate = await User.findOne({ email });
+        if (!userToUpdate) {
+            return res.status(404).send({ message: "Email not present!" });
+        }
+
+        // Manual password hashing may be needed here if password is updated
+
+        const updatedUser = await User.updateOne({ email }, req.body);
+        return res.status(200).send({ data: 'User Updated!' });
+    } catch (error) {
+        return res.status(500).send({ message: 'Error updating user.' });
+    }
 };
--- models/userSchema.js
+++ models/userSchema.js
@@ -36,16 +36,5 @@ userSchema.pre("save", async function (next) {
     this.password = await bcrypt.hash(this.password, 10);
 });
 
-// //COMPARING THE USER PASSWORD ENTERED BY USER WITH THE USER SAVED PASSWORD
-// userSchema.methods.comparePassword = async function (enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
-
-// //GENERATING A JWT TOKEN WHEN A USER REGISTERS OR LOGINS, IT DEPENDS ON OUR CODE THAT WHEN DO WE NEED TO GENERATE THE JWT TOKEN WHEN THE USER LOGIN OR REGISTER OR FOR BOTH. 
-// userSchema.methods.getJWTToken = function () {
-//   return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
-//     expiresIn: process.env.JWT_EXPIRES,
-//   });
-// };
 
 export const User = mongoose.model("User", userSchema);

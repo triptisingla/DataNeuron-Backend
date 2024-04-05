1ded66d8ddd21c62bd98825412c68ed95adb9d61
triptisingla <triptisingla306@gmail.com>
2024-04-06 01:13:41 +0530

    test

--- models/userSchema.js
+++ models/userSchema.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, "Please provide a Password!"],
-        minLength: [8, "Password must contain at least 8 characters!"],
+        minLength: [8, "Password must contain at least 4 characters!"],
         maxLength: [32, "Password cannot exceed 32 characters!"],
         select: false,
     },

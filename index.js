43029c8c39842c92416812df45fd543042442b32
triptisingla <triptisingla306@gmail.com>
2024-04-05 22:08:33 +0530

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-
+const PORT=process.env.PORT||8000
 app.listen(process.env.PORT, () => {
-  console.log(`Server running at port ${process.env.PORT}`);
+  console.log(`Server running at port ${PORT}`);
 });
